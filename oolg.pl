8544042a3c6b022d1b07bf5e09d8c6bd10ce21ec
syanuma <satoshi.yanuma@gmail.com>
2013-07-15 10:03:54 +0900

    perlcriticに従いconstの設定。インデントの調整。

--- oolg.pl
+++ oolg.pl
@@ -2,23 +2,26 @@ use strict;
 use warnings;
 use Time::HiRes;
 use LifeGame::Field;
+use Const::Fast;
 
-my $MAX_LOW = my $MAX_COLUMN = 40;
-my $WAIT_TIME = 0.1;
+const my $MAX_LOW = 40;
+const my $MAX_COLUMN = 40;
+const my $WAIT_TIME = 0.1;
+const my $LOOP_TIME = 200;
 
-system('clear');
+system 'clear';
 
 my $fs = LifeGame::Field->new(
-  {
-    MAX_LOW => $MAX_LOW,
-    MAX_COLUMN => $MAX_COLUMN,
-  }
+    {
+        MAX_LOW    => $MAX_LOW,
+        MAX_COLUMN => $MAX_COLUMN,
+    }
 );
 
-foreach my $i ( 1 .. 200 ) {
-  Time::HiRes::sleep $WAIT_TIME;
-  $fs->show->check_life;
-  print "$i G\n";
+foreach my $i ( 1 .. $LOOP_TIME ) {
+    Time::HiRes::sleep $WAIT_TIME;
+    $fs->show->check_life;
+    print "$i G\n";
 }
 
 print "\nend Life Game\n";
